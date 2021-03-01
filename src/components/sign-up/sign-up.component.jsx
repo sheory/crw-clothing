95978d6a7998c8c4220fc47b4ea010aaadbdc078
sheory <sheoryd@gmail.com>
2021-03-01 12:05:50 -0300

    implemented firebase utils, included to store authenticated users into our firestore database

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -2,36 +2,110 @@ import React from 'react';
 
 import './sign-up.styles.scss'
 
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component,jsx";
+
+import { auth,createUserProfileDocument } from "../../firebase/firebase.utils";
+
 class SignUp extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
+      displayName: '',
       email: '',
-      password: ''
+      password: '',
+      confirmPassword: ''
     }
   }
 
+  componentDidMount() {
+
+  }
+
+  handleSubmit = async e => {
+    console.log("hey im here")
+    e.preventDefault()
+
+   const {displayName, email, password, confirmPassword} = this.state;
+
+    if(password !== confirmPassword) {
+      console.log("hey, they differnet")
+      alert("Passwords don't match");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+         email,
+         password
+      )
+
+      await createUserProfileDocument(user, {displayName});
+
+      this.setState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  handleChange = e => {
+    const { value, name } = e.target
+    this.setState({ [name]: value })
+  }
+
+
   render() {
+    const {displayName, email, password, confirmPassword} = this.state;
+
     return (
-       <div>
-         <h1>I don't have an account</h1>
-         <p>Sign up with your email and password</p>
-         <form >
-           <label htmlFor="">Display Name</label>
-           <input type='text'/>
-           <label htmlFor="">Email</label>
-           <input type='text' onChange={(e) => (this.setState({email: e.target.value}))}/>
-           <p>{this.state.email}</p>
-           <label htmlFor="">Password</label>
-           <input type='password' onChange={(e) => (this.setState({password: e.target.value}))}/>
-           <p>{this.state.password}</p>
-           <label htmlFor="">Confirm Password</label>
-           <input type='password' onChange={(e) => (this.setState({password: e.target.value}))}/>
-           <p>{this.state.password}</p>
-         </form>
-         <button>SIGN UP</button>
+       <div className='sign-up'>
+         <h2 className='title'>I don't have an account</h2>
+         <span>Sign up with your email and password</span>
+          <form className='sign-up-form' onSubmit={this.handleSubmit }>
+            <FormInput
+               name='displayName'
+               type='text'
+               label='Display Name'
+               handleChange={this.handleChange}
+               value={displayName}
+               required
+            />
+            <FormInput
+            name='email'
+            type='email'
+            label='Email'
+            handleChange={this.handleChange}
+            value={email}
+            required
+            />
+            <FormInput
+               name='password'
+               type='password'
+               label='Password'
+               handleChange={this.handleChange}
+               value={password}
+               required
+            />
+            <FormInput
+               name='confirmPassword'
+               type='password'
+               label='Confirm Password'
+               handleChange={this.handleChange}
+               value={confirmPassword}
+               required
+            />
+
+            <CustomButton type='submit' >
+              Sign in
+            </CustomButton>
+          </form>
        </div>
 
     )
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -8,6 +8,7 @@ import SignUp from '../../components/sign-up/sign-up.component';
 const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
